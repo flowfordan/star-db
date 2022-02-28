7ded8954f123e48e9c50adef6162a1e904081afd
flowfordan <flowfordan@gmail.com>
2022-02-28 18:00:26 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+class SwapiService {
+
+  _apiBase = 'https://swapi.dev/api'
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Couldn't fetch ${url}` + `, recieved ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllPeople() {
+    return this.getResource(`/people/`);
+  };
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  };
+
+};
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((body) => {
+  console.log(body)
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
     </div>
   );
 }
