e4a2db66c760dc8252b83aeff7dc4790fd1b4b14
flowfordan <flowfordan@gmail.com>
2022-03-03 11:41:24 +0300

    styles adjusments

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -1,24 +1,57 @@
 import styles from './RandomPlanet.module.css'
 import { Box } from '@mui/material';
 import globalStyles from '../../style/globalStyles.module.css'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 
 const RandomPlanet = () => {
+
+    //population
+    //rotation
+    //diameter
+
+
+
+
     return (
         <div className={styles.cardWrapper}>
             <div className={styles.cardHeader}>
                 Random Planet
             </div>
             <Box className={`${styles.card} ${globalStyles.basicBox}`}>
-                <div className={styles.cardName}>
+                <div className={`${styles.cardName} ${globalStyles.typoItemName}`}>
                     Name
                 </div>
+
                 <div className={styles.cardImage}>
-                    Picture
+                    <img  className={styles.cardImageBody} src='https://www.w3schools.com/css/paris.jpg'>
+                    </img>
                 </div>
-                <div className={styles.cardInfo}>
-                    Information
+
+                <div className={`${styles.cardInfo} ${globalStyles.typoItemsInfo}`}>
+                    <ul className={globalStyles.infoList}>
+
+                        <li className={globalStyles.infoListGroup}>
+                            <span className={globalStyles.infoListTerm}>Population</span>
+                            <span className={globalStyles.infoListEl}>1234</span>
+                        </li>
+                        <li className={globalStyles.infoListGroup}>
+                            <span className={globalStyles.infoListTerm}>Rotation Period</span>
+                            <span className={globalStyles.infoListEl}>43</span>
+                        </li>
+                        <li className={globalStyles.infoListGroup}>
+                            <span className={globalStyles.infoListTerm}>Diameter</span>
+                            <span className={globalStyles.infoListEl}>999</span>
+                        </li>
+
+                    </ul>
                 </div>
+
             </Box>
         </div>
     )
