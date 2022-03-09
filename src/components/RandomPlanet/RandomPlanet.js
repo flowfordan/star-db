f80234cc061d2b465601713eb0817d39ae83b4b1
flowfordan <flowfordan@gmail.com>
2022-03-09 12:09:32 +0300

    added preloader to randomplanet component

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -9,13 +9,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import React from 'react';
 import SwapiService from '../../services/swapiService';
+import Spinner from '../Spinner/Spinner';
 
 
 export default class RandomPlanet extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            planet: {}
+            planet: {},
+            isLoading: true,
           };
 
         this.updatePlanet()
@@ -24,7 +26,8 @@ export default class RandomPlanet extends React.Component {
     swapiService = new SwapiService();
 
     onPlanetLoaded = (planet) => {
-        this.setState({planet})
+        this.setState({planet,
+        isLoading: false})
     };
 
     updatePlanet(){
@@ -35,58 +38,121 @@ export default class RandomPlanet extends React.Component {
 
     render(){
 
-        const {planet: {id, name, population, 
-            rotationPeriod, diameter}} = this.state;
+        const {planet, isLoading} = this.state;
+        
+        const PlanetView = ({ planet }) => {
+
+            const {id, name, population, 
+                rotationPeriod, diameter} = planet;
+
+            return(
+                <React.Fragment>
+                    <Box className={`${styles.card} ${globalStyles.basicBox}`}>
+                        <div className={`${styles.cardName} ${globalStyles.typoItemName}`}>
+                            {name}
+                        </div>
+        
+                        <div className={styles.cardImage}>
+                            <img  className={styles.cardImageBody}
+                            alt="Image of Planet" 
+                            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}>
+                            </img>
+                        </div>
+        
+                        <div className={`${styles.cardInfo} ${globalStyles.typoItemsInfo}`}>
+                            <ul className={globalStyles.infoList}>
+        
+                                <li className={globalStyles.infoListGroup}>
+                                    <span className={globalStyles.infoListTerm}>
+                                        Population
+                                    </span>
+                                    <span className={globalStyles.infoListEl}>
+                                        {population}
+                                    </span>
+                                </li>
+                                <li className={globalStyles.infoListGroup}>
+                                    <span className={globalStyles.infoListTerm}>
+                                        Rotation Period
+                                    </span>
+                                    <span className={globalStyles.infoListEl}>
+                                        {rotationPeriod}
+                                    </span>
+                                </li>
+                                <li className={globalStyles.infoListGroup}>
+                                    <span className={globalStyles.infoListTerm}>
+                                        Diameter
+                                    </span>
+                                    <span className={globalStyles.infoListEl}>
+                                        {diameter}
+                                    </span>
+                                </li>
+        
+                            </ul>
+                        </div>
+        
+                    </Box>
+
+
+                </React.Fragment>
+            )
+        };
+
+        const PreloaderView = () => {
+            return(
+                <React.Fragment>
+                    <Box className={`${styles.card} ${globalStyles.basicBox}`}>
+
+                        <div className={`${styles.cardName} ${globalStyles.typoItemName}`}>
+                            <Spinner/>
+                        </div>
+        
+                        <div className={styles.cardImage}>
+                            <Spinner/>
+                        </div>
+        
+                        <div className={`${styles.cardInfo} ${globalStyles.typoItemsInfo}`}>
+                            <Spinner/>
+                        </div>
+        
+                    </Box>
+                </React.Fragment>
+            )
+        };
+
+        const renderedContent = isLoading? <PreloaderView/> : <PlanetView planet={planet}/>;
 
         return (
             <div className={styles.cardWrapper}>
-                <div className={styles.cardHeader}>
-                    Random Planet
-                </div>
-                <Box className={`${styles.card} ${globalStyles.basicBox}`}>
-                    <div className={`${styles.cardName} ${globalStyles.typoItemName}`}>
-                        {name}
-                    </div>
+                         <div className={styles.cardHeader}>
+                             Random Planet
+                         </div>
     
-                    <div className={styles.cardImage}>
-                        <img  className={styles.cardImageBody} 
-                        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}>
-                        </img>
-                    </div>
-    
-                    <div className={`${styles.cardInfo} ${globalStyles.typoItemsInfo}`}>
-                        <ul className={globalStyles.infoList}>
-    
-                            <li className={globalStyles.infoListGroup}>
-                                <span className={globalStyles.infoListTerm}>
-                                    Population
-                                </span>
-                                <span className={globalStyles.infoListEl}>
-                                    {population}
-                                </span>
-                            </li>
-                            <li className={globalStyles.infoListGroup}>
-                                <span className={globalStyles.infoListTerm}>
-                                    Rotation Period
-                                </span>
-                                <span className={globalStyles.infoListEl}>
-                                    {rotationPeriod}
-                                </span>
-                            </li>
-                            <li className={globalStyles.infoListGroup}>
-                                <span className={globalStyles.infoListTerm}>
-                                    Diameter
-                                </span>
-                                <span className={globalStyles.infoListEl}>
-                                    {diameter}
-                                </span>
-                            </li>
-    
-                        </ul>
-                    </div>
-    
-                </Box>
-            </div>
+                        {renderedContent}
+
+                     </div>
         )
+
+        // if(isLoading){
+        //     return (
+        //         <div className={styles.cardWrapper}>
+        //             <div className={styles.cardHeader}>
+        //                 Random Planet
+        //             </div>
+
+        //             <PreloaderView />
+        //         </div>
+        //     )
+        // };
+           
+        // return (
+        //     <div className={styles.cardWrapper}>
+        //             <div className={styles.cardHeader}>
+        //                 Random Planet
+        //             </div>
+
+        //             <PlanetView planet={planet}/>
+        //     </div>
+        // );
+
     }
-};
+}
