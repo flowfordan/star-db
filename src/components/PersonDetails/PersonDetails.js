c2aa4e85dcd4f5216db51c5a5a5bbad3686497d0
flowfordan <flowfordan@gmail.com>
2022-03-10 11:31:27 +0300

    character details adjusments

--- src/components/PersonDetails/PersonDetails.js
+++ src/components/PersonDetails/PersonDetails.js
@@ -3,6 +3,8 @@ import { Box } from '@mui/material';
 import globalStyles from '../../style/globalStyles.module.css'
 import React from 'react';
 import SwapiService from '../../services/swapiService';
+import Spinner from '../Spinner/Spinner';
+import ErrorIndicator from '../ErrorIndicator/ErrorIndicator';
 
 
 export default class PersonDetails extends React.Component{
@@ -11,6 +13,8 @@ export default class PersonDetails extends React.Component{
     
     this.state = {
         person: null,
+        isLoading: true,
+        error: false
     }
     };
 
@@ -23,7 +27,8 @@ export default class PersonDetails extends React.Component{
 
     componentDidUpdate(prevProps){
         if(this.props.personId != prevProps.personId){
-           this.updatePerson(); 
+           this.updatePerson();
+           this.setState({isLoading: true})
         }
         
     };
@@ -37,8 +42,14 @@ export default class PersonDetails extends React.Component{
 
         this.swapiService.getPerson(personId)
         .then((person) => {
-            this.setState({person})
-        })
+            this.setState({person, isLoading: false})
+        }).catch(this.onError);
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+        isLoading: false})
     };
 
     render(){
@@ -49,57 +60,89 @@ export default class PersonDetails extends React.Component{
             )
         }
 
-        const {id, name, gender, birthYear, eyeColor} = this.state.person;
+        
 
-        return(
-            <div className={styles.cardWrapper}>
-            <div className={styles.cardHeader}>
-                Character
-            </div>
-            <Box className={`${styles.card} ${globalStyles.basicBox}`}>
+        const CharacterView = () => {
+
+            const {id, name, gender, birthYear, eyeColor} = this.state.person;
+
+            return(
+                <React.Fragment>
+                    <Box className={`${styles.card} ${globalStyles.basicBox}`}>
                 <div className={`${styles.cardName} ${globalStyles.typoItemName}`}>
                     {name}
                 </div>
                 <div className={styles.cardImage}>
-                <img  className={styles.cardImageBody}
-                src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-                alt="Character">
-                </img>
+                    <img  className={styles.cardImageBody}
+                    src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+                    alt="Character">
+                    </img>
                 </div>
                 
                 <div className={`${styles.cardInfo} ${globalStyles.typoItemsInfo}`}>
-                            <ul className={globalStyles.infoList}>
+                    <ul className={globalStyles.infoList}>
         
-                                <li className={globalStyles.infoListGroup}>
-                                    <span className={globalStyles.infoListTerm}>
-                                        Gender
-                                    </span>
-                                    <span className={globalStyles.infoListEl}>
-                                        {gender}
-                                    </span>
-                                </li>
-                                <li className={globalStyles.infoListGroup}>
-                                    <span className={globalStyles.infoListTerm}>
-                                        Birth Year
-                                    </span>
-                                    <span className={globalStyles.infoListEl}>
-                                        {birthYear}
-                                    </span>
-                                </li>
-                                <li className={globalStyles.infoListGroup}>
-                                    <span className={globalStyles.infoListTerm}>
-                                        Eye Color
-                                    </span>
-                                    <span className={globalStyles.infoListEl}>
-                                        {eyeColor}
-                                    </span>
-                                </li>
+                        <li className={globalStyles.infoListGroup}>
+                            <span className={globalStyles.infoListTerm}>
+                                Gender
+                            </span>
+                            <span className={globalStyles.infoListEl}>
+                                {gender}
+                            </span>
+                        </li>
+                        <li className={globalStyles.infoListGroup}>
+                            <span className={globalStyles.infoListTerm}>
+                                Birth Year
+                            </span>
+                            <span className={globalStyles.infoListEl}>
+                                {birthYear}
+                            </span>
+                        </li>
+                        <li className={globalStyles.infoListGroup}>
+                            <span className={globalStyles.infoListTerm}>
+                                Eye Color
+                            </span>
+                            <span className={globalStyles.infoListEl}>
+                                {eyeColor}
+                            </span>
+                        </li>
         
-                            </ul>
-                        </div>
+                    </ul>
+                </div>
 
             </Box>
-        </div> 
+
+                </React.Fragment>
+            )
+        };
+
+        const PreloaderView = () => {
+            return(
+                <React.Fragment>
+                    <Box className={globalStyles.basicBox}>
+                        <Spinner />
+                    </Box>
+                </React.Fragment>
+
+            )
+        };
+
+        const {isLoading, error} = this.state;
+
+        const hasData = !(isLoading || error);
+        const errorMessage = error? <ErrorIndicator /> : null;
+        const loading = isLoading? <PreloaderView /> : null;
+        const renderedContent = !hasData? null : <CharacterView />;
+
+        return(
+            <div className={styles.cardWrapper}>
+            <div className={`${globalStyles.typoChunkHeader} ${styles.cardHeader}`}>
+                Character
+            </div>
+                {errorMessage}
+                {loading}
+                {renderedContent}          
+            </div> 
         )
     }
 
